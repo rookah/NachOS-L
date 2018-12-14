deb55781d06ec5bf95bb4e74219e500af76730fc
CaramelDunes <tamalulu@hotmail.fr>
2018-12-14 11:19:02 +0100

    Cleanup jointest

--- code/test/jointest.c
+++ code/test/jointest.c
@@ -2,50 +2,42 @@
 
 void thread3(void *d) {
 
-  // int i=0;
-  // PutInt(8);
   for (int i = 0; i < 5; i++) {
-    // while(1){
-    PutChar('p');
-    // PutChar('a');
+    PutChar('3');
   }
+
   UserThreadExit();
-  // PutChar('g');
 }
 
-void thread(void *d) {
+void thread1(void *d) {
 
-  // int i=0;
-  // PutInt(8);
-  for (int i = 0; i < 100; i++) {
-    // while(1){
-    PutChar('g');
-    // PutChar('a');
+  for (int i = 0; i < 5; i++) {
+    PutChar('1');
   }
+
   UserThreadExit();
-  // PutChar('g');
 }
 void thread2(void *d) {
+
   int id3 = UserThreadCreate(thread3, (void *)0);
 
-  // int i=0;
-  // PutInt(8);
   for (int i = 0; i < 5; i++) {
-    // while(1){
-    PutChar('t');
-    // PutChar('a');
+    PutChar('2');
   }
+
   UserThreadJoin(id3);
   UserThreadExit();
-  // PutChar('g');
 }
 
 int main() {
 
-  int id1 = UserThreadCreate(thread2, (void *)0);
-  int id2 = UserThreadCreate(thread, (void *)0);
+  int id1 = UserThreadCreate(thread1, (void *)0);
+  int id2 = UserThreadCreate(thread2, (void *)0);
+
   UserThreadJoin(id1);
   UserThreadJoin(id2);
+
+  PutChar('\n');
 
   Halt();
   return 0;
