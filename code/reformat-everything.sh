a8b56c2b42cb860214ebbd94734deab9dc741b1c
devnoname120 <devnoname120+gh@gmail.com>
2018-12-14 10:58:44 +0100

    Formatting changes

--- code/reformat-everything.sh
+++ code/reformat-everything.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env sh
 
-find . -iname '*.cc' -o -iname '*.h' -o -iname '*.cpp' -o -iname '*.c' | xargs clang-format -i
+find . -iname '*.cc' -o -iname '*.h' -o -iname '*.cpp' -o -iname '*.c' | xargs clang-format -i -style=file
