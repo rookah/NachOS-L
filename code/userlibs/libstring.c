9af4c873d6f06bc5613cd8a8cf1d048defbf5147
rookah <gisselairel@gmail.com>
2019-01-15 11:30:25 +0100

    add user libstring

---
+++ code/userlibs/libstring.c
@@ -0,0 +1,19 @@
+#include "syscall.h"
+#include <stddef.h>
+
+int streq(char* s1, char* s2) {
+	if ((s1 == NULL && s2 != NULL) || (s1 != NULL && s2 == NULL))
+		return 0;
+
+	int i = 0;
+	while (s1[i] != '\0') {
+		if (s2[i] == '\0')
+			return 0;
+		i++;
+	}
+
+	if (s2[i] == '\0')
+		return 1;
+	else
+		return 0;
+}
---
+++ code/userlibs/libstring.h
@@ -0,0 +1,3 @@
+#include "syscall.h"
+
+int streq(char* s1, char* s2); // returns 1 if s1 and s2 are equal, else returns 0
